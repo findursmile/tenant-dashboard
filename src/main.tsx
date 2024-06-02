d9b1b6205384d1b8a01f6929980566389ae7ff8a
Mohan Raj <praem1990@gmail.com>
2024-06-02 13:37:01 +0530

    Add axios interceptor to add bearer token

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import './common/axios';
 import './css/style.css';
 import './css/satoshi.css';
 import 'jsvectormap/dist/css/jsvectormap.css';
