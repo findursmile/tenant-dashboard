744a7d4c00ff4c713b61c5fb72de52220631bdcd
Mohan Raj <praem1990@gmail.com>
2024-11-30 23:58:08 +0530

    Cleanup Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -38,48 +38,16 @@ import {
   Square2StackIcon,
   TicketIcon,
 } from '@heroicons/react/20/solid'
+import { Button } from '../catalyst/button'
 
 function Example() {
   return (
     <Sidebar>
       <SidebarHeader>
-        <Dropdown>
-          <DropdownButton as={SidebarItem} className="mb-2.5">
+          <Button plain as={SidebarItem} className="mb-2.5 pl-0">
             <Avatar src="/src/images/logo/logo-icon.svg" />
-            <SidebarLabel>Tailwind Labs</SidebarLabel>
-            <ChevronDownIcon />
-          </DropdownButton>
-          <DropdownMenu className="min-w-64" anchor="bottom start">
-            <DropdownItem href="/teams/1/settings">
-              <Cog8ToothIcon />
-              <DropdownLabel>Settings</DropdownLabel>
-            </DropdownItem>
-            <DropdownDivider />
-            <DropdownItem href="/teams/1">
-              <Avatar slot="icon" src="/tailwind-logo.svg" />
-              <DropdownLabel>Tailwind Labs</DropdownLabel>
-            </DropdownItem>
-            <DropdownItem href="/teams/2">
-              <Avatar slot="icon" initials="WC" className="bg-purple-500 text-white" />
-              <DropdownLabel>Workcation</DropdownLabel>
-            </DropdownItem>
-            <DropdownDivider />
-            <DropdownItem href="/teams/create">
-              <PlusIcon />
-              <DropdownLabel>New team&hellip;</DropdownLabel>
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        <SidebarSection>
-          <SidebarItem href="/search">
-            <MagnifyingGlassIcon />
-            <SidebarLabel>Search</SidebarLabel>
-          </SidebarItem>
-          <SidebarItem href="/inbox">
-            <InboxIcon />
-            <SidebarLabel>Inbox</SidebarLabel>
-          </SidebarItem>
-        </SidebarSection>
+            <SidebarLabel>FindUrSmile</SidebarLabel>
+          </Button>
       </SidebarHeader>
       <SidebarBody>
         <SidebarSection>
@@ -91,30 +59,8 @@ function Example() {
             <Square2StackIcon />
             <SidebarLabel>Events</SidebarLabel>
           </SidebarItem>
-          <SidebarItem href="/orders">
-            <TicketIcon />
-            <SidebarLabel>Orders</SidebarLabel>
-          </SidebarItem>
-          <SidebarItem href="/broadcasts">
-            <MegaphoneIcon />
-            <SidebarLabel>Broadcasts</SidebarLabel>
-          </SidebarItem>
-          <SidebarItem href="/settings">
-            <Cog6ToothIcon />
-            <SidebarLabel>Settings</SidebarLabel>
-          </SidebarItem>
         </SidebarSection>
         <SidebarSpacer />
-        <SidebarSection>
-          <SidebarItem href="/support">
-            <QuestionMarkCircleIcon />
-            <SidebarLabel>Support</SidebarLabel>
-          </SidebarItem>
-          <SidebarItem href="/changelog">
-            <SparklesIcon />
-            <SidebarLabel>Changelog</SidebarLabel>
-          </SidebarItem>
-        </SidebarSection>
       </SidebarBody>
       <SidebarFooter>
         <Dropdown>
@@ -138,15 +84,6 @@ function Example() {
             <DropdownItem href="/settings">
               <Cog8ToothIcon />
               <DropdownLabel>Settings</DropdownLabel>
-            </DropdownItem>
-            <DropdownDivider />
-            <DropdownItem href="/privacy-policy">
-              <ShieldCheckIcon />
-              <DropdownLabel>Privacy policy</DropdownLabel>
-            </DropdownItem>
-            <DropdownItem href="/share-feedback">
-              <LightBulbIcon />
-              <DropdownLabel>Share feedback</DropdownLabel>
             </DropdownItem>
             <DropdownDivider />
             <DropdownItem href="/logout">
