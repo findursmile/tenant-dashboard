4d44948f8c0c7cadb3b82a65804f997d463a27bf
Mohan Raj <praem1990@gmail.com>
2024-12-06 12:16:14 +0530

    Make the SignIn page covers the entire screen

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -52,7 +52,7 @@ const SignIn: React.FC = () => {
   return (
     <>
       <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-        <div className="flex flex-wrap items-center">
+        <div className="flex flex-wrap h-screen items-center justify-center">
           <div className="hidden w-full xl:block xl:w-1/2">
             <div className="py-17.5 px-26 text-center">
               <Link className="mb-5.5 inline-block" to="/">
